18c0592490ce0167bb7dafe81876b497a77706c1
Letícia Sayuri <sayurimiuki@gmail.com>
2023-12-29 20:23:09 -0300

    Desafio concluído!

--- Program.cs
+++ Program.cs
@@ -1,3 +1,15 @@
-﻿using DesafioPOO.Models;
+﻿using System.ComponentModel;
+using DesafioPOO.Models;
 
-// TODO: Realizar os testes com as classes Nokia e Iphone
+// TODO: Realizar os testes com as classes Nokia e Iphone*OK*
+Console.WriteLine("Smartphone Nokia:");
+Smartphone nokia = new Nokia(numero: "654321", modelo: "Modelo 10", imei: "5555555", memoria: 77);
+nokia.Ligar();
+nokia.InstalarAplicativo("Whatsapp");
+
+Console.WriteLine("\n");
+
+Console.WriteLine("Smartphone iPhone:");
+Smartphone iphone = new Iphone(numero: "4545", modelo: "Modelo 20", imei: "8888888", memoria: 130);
+iphone.ReceberLigacao();
+iphone.InstalarAplicativo("Telegram");
